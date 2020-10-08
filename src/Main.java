3e746b6fdd512567259c9c3ef30eeeff31330355
Akomi03 <55061661+Akomi03@users.noreply.github.com>
2020-10-08 23:20:33 +0200

    Pc

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-	 Date datum1 = new Date(2019, 11, 12);
+	 Datenstruktur data = new Datenstruktur();
     }
 }
