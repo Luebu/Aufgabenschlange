76b5704ab5a5117da91e99692bc2dcc91413abdb
Luebu <maximilian.luecker@hebk.de>
2020-10-09 09:19:14 +0200

    Fertig

--- src/Datenstruktur.java
+++ src/Datenstruktur.java
@@ -5,24 +5,59 @@ public class Datenstruktur {
     private Knoten first;
 
     public void hinzufuegen(int pZahl) {
+        System.out.println("Welchen Inhalt hat die Aufgabe?");
+        String text = sc.next();
+        System.out.println("Welches Jahr soll das Datum haben?");
+        int j = sc.nextInt();
+        System.out.println("Welchen Monat soll das Datum haben?");
+        int m = sc.nextInt();
+        System.out.println("Welchen Tag soll das Datum haben?");
+        int t = sc.nextInt();
         if (first == null) {
-            first = new Knoten(sc.next(), sc.nextInt(), sc.nextInt(), sc.nextInt());
+            first = new Knoten(text, j, m, t);
         } else {
             Knoten aE = first;
             for (int i = 0; i < pZahl; i++) {
                 if (aE.getNachfolger() != null) {
                     aE = aE.getNachfolger();
                 }
-                aE.setNachfolger(new Knoten(sc.next(), sc.nextInt(), sc.nextInt(), sc.nextInt()));
+            }
+            Knoten temp = new Knoten(text, j, m, t);
+            if (aE.getNachfolger() != null) {
+                temp.setNachfolger(aE.getNachfolger().getNachfolger());
+                aE.setNachfolger(temp);
+            }else{
+                aE.setNachfolger(temp);
             }
         }
     }
 
-    public void suchen(String pText) {
-
+    public void suchen() {
+        if (first == null) {
+            System.out.println("Keine gespeicherten Daten vorhanden");
+            return;
+        }
+        Knoten aE = first;
+        while (aE.getNachfolger() != null) {
+            System.out.println(aE.getInhalt().getText());
+            System.out.println(aE.getInhalt().getDatum());
+            aE = aE.getNachfolger();
+        }
     }
 
-    public boolean loeschen(Aufgabe pAufgabe) {
+    public boolean loeschen(int pZahl) {
+        if (first == null) {
+            return false;
+        }
+        Knoten aE = first;
+        for (int i = 0; i < pZahl - 1; i++) {
+            if (aE.getNachfolger() != null) {
+                aE = aE.getNachfolger();
+            } else {
+                return false;
+            }
+        }
+        aE.setNachfolger(aE.getNachfolger().getNachfolger());
         return true;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,43 @@
-import java.util.Date;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	 Datenstruktur data = new Datenstruktur();
+    public static void main(String[] args) throws InterruptedException {
+        Datenstruktur data = new Datenstruktur();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Wilkommen bei ihrem Aufgabenspeicher");
+            System.out.println("Was möchten sie machen?");
+            System.out.println("Drücken sie 1 um eine neue Aufgabe hinzuzufügen");
+            System.out.println("Drücken sie 2 um ihre Aufgaben anzusehen");
+            System.out.println("Oder drücken sie 3 um eine Aufgabe zu löschen");
+            switch (sc.nextInt()) {
+                case 1:
+                    System.out.println("An welcher Stelle soll die Aufgabe hinzugefügt werden?");
+                    data.hinzufuegen(sc.nextInt());
+                    break;
+                case 2:
+                    data.suchen();
+                    System.out.println();
+                    System.out.println("Geben sie etwas ein um fortzufahren");
+                    sc.next();
+                    break;
+                case 3:
+                    System.out.println("Welche Aufgabe soll geloescht werden");
+                    if(data.loeschen(sc.nextInt())){
+                      System.out.println("Erfolgreich gelöescht");
+                    }else{
+                        System.out.println("Es wurde nichts gelöscht.");
+                        System.out.println("Versuchen sie eine andere Zahl");
+                    }
+                    break;
+                default:
+                    System.out.println("Keine Auswahlmöglichkeit");
+                    System.out.println("Versuchen sie es erneut!");
+            }
+            System.out.println("Sie kehren nun automatisch zum start zurück");
+            Thread.sleep(5000);
+            System.out.print("\033[H\033[2J");
+        }
     }
 }
